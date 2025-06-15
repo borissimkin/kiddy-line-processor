48154934e830e971f54ff680bd87cdadf79d96f8
borissimkin <simkinbb@gmail.com>
2025-06-15 23:14:06 +0300

    add proto

--- internal/service/sport_test.go
+++ internal/service/sport_test.go
@@ -9,7 +9,7 @@ func TestHelloName(t *testing.T) {
 	name := "Gladys"
 	want := regexp.MustCompile(`\b` + name + `\b`)
 	msg, err := func() (string, error) {
-		return "awd", nil
+		return "Gladys", nil
 	}()
 	if !want.MatchString(msg) || err != nil {
 		t.Errorf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
